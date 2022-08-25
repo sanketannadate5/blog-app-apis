457509be280dab3b7f26f694866f3789ea9f03be
SanketAnnadate <sanketannadatejain@gmail.com>
2022-08-25 18:16:54 +0530

    spring security updated according to the new implementation

--- src/main/java/com/sanket/blogappapis/config/SecurityConfig.java
+++ src/main/java/com/sanket/blogappapis/config/SecurityConfig.java
@@ -3,16 +3,16 @@ package com.sanket.blogappapis.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -24,7 +24,7 @@ import com.sanket.blogappapis.security.JwtAuthenticationFilter;
 @EnableWebSecurity
 @EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 	
 	private static final String[] PUBLIC_URLS = {
 			"/api/auth/**",
@@ -43,15 +43,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	JwtAuthenticationFilter jwtAuthenticationFilter;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(customUserDetailservice).passwordEncoder(getPasswordEncoder());
+	
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setUserDetailsService(customUserDetailservice);
+		daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
+		return daoAuthenticationProvider;
 	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		
 		http.csrf().disable()
+		.authenticationProvider(daoAuthenticationProvider())
 		.authorizeHttpRequests().antMatchers(PUBLIC_URLS).permitAll()
 //		.antMatchers(HttpMethod.GET).permitAll()  TO PERMIT ALL GET METHODS WITHOUT AUTHENTICATION
 		.anyRequest().authenticated()
@@ -61,64 +67,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()
 		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();
 	}
-	
+	  
 	@Bean
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
 	
 	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-	
-	
+	public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}	
 }
-
-
-
-
-
-
-
-
-
-//package com.sanket.blogappapis.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import com.sanket.blogappapis.security.CustomUserDetailservice;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//	@Autowired
-//	CustomUserDetailservice customUserDetailservice;
-//
-//	@Bean
-//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//		http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated().and().httpBasic()
-//		.and().userDetailsService(customUserDetailservice).;
-//		return http.build();
-//	}
-//
-//	@Bean
-//    public PasswordEncoder getPasswordEncoder(){
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
-//}
